25b4d37da60b3e882274133bf5f69a8f5186ad33
Isiaka Abdulahi <isiakaabd@gmail.com>
2022-05-18 17:40:20 +0100

    added

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}", "dist/index.html", "./dist/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
